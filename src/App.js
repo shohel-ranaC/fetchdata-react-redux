e514ce05b9f05bbdad029f0b95dcf560dff54a68
shohel-ranaC <akashshohel422@gmail.com>
2022-11-20 03:49:55 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
 
 export default App;
+
+// step1: install package
+// step2: constants define
+// step3: async action creator - for install redux-thunk
+// step4: reducer
+// step5: create store
+// step6: provide store
+// step7: use store
+// step8: adding css
+
